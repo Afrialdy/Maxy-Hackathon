b8aa5a51dbed74146662794ac3bcd185bf796974
aldi6676 <aldisyaputra6676@gmail.com>
2024-04-23 20:23:28 +0700

    add popup signup and login

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,3 +249,23 @@
   });
 
 })()
+
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+
+/* Show form Popup */
+showPopupBtn.addEventListener("click", () => {
+  document.body.classList.toggle("show-popup");
+})
+
+/* Hide form Popup */
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click())
+
+loginSignupLink.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+  })
+})
